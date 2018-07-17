7b86609911aa594644a177a0dbf60c2ea18c0418
oliviakris24 <okfishy@gmail.com>
2018-07-17 18:18:46 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/' do
+  "Hello World!"
+end 
+
+get '/name' do
+  "My name is Olivia"
+end 
+
+get '/hometown' do
+  "My hometown is Los Angeles"
+end 
+
+get '/favorite-song' do
+  "My favorite song is The Wire by HAIM"
+end 
+
 end
+
